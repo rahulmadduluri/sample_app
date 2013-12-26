14b5199a57b54c623395c1ace7ee15092ca34fe2
Rahul Madduluri <rahulmadduluri@gmail.com>
2013-12-25 18:30:09 -0800

    Finish initial stages of user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do 
+	factory :user do
+		name					"Rahul Madduluri"
+		email					"rahulmadduluri@gmail.com"
+		password				"foobar"
+		password_confirmation	"foobar"
+	end
+	
+end
